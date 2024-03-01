83660313b1b73c79d115af5fc26b920ef3d49ec4
Stygian84 <nicholasgandhi84@gmail.com>
2024-03-01 10:24:05 +0800

    Lab1 Ex2.4c RecordId -Run RecordIdTest with 0 errors

--- src/java/simpledb/storage/RecordId.java
+++ src/java/simpledb/storage/RecordId.java
@@ -19,24 +19,26 @@ public class RecordId implements Serializable {
      * @param tupleno
      *            the tuple number within the page.
      */
+    private final PageId pid;
+    private final int tupleno;
+
     public RecordId(PageId pid, int tupleno) {
-        // some code goes here
+        this.pid = pid;
+        this.tupleno = tupleno;
     }
 
     /**
      * @return the tuple number this RecordId references.
      */
     public int getTupleNumber() {
-        // some code goes here
-        return 0;
+        return tupleno;
     }
 
     /**
      * @return the page id this RecordId references.
      */
     public PageId getPageId() {
-        // some code goes here
-        return null;
+        return pid;
     }
 
     /**
@@ -47,8 +49,10 @@ public class RecordId implements Serializable {
      */
     @Override
     public boolean equals(Object o) {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        if (this == o) return true;
+        if (!(o instanceof RecordId)) return false;
+        RecordId recordId = (RecordId) o;
+        return tupleno == recordId.tupleno && pid.equals(recordId.pid);
     }
 
     /**
@@ -59,9 +63,9 @@ public class RecordId implements Serializable {
      */
     @Override
     public int hashCode() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
+        int result = pid.hashCode();
+        result = 31 * result + tupleno;
+        return result;
     }
 
 }
